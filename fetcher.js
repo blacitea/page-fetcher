84265da2af9f588226190700f2896fbad3427d92
Angel Tsang <blacitea@gmail.com>
2020-07-30 01:18:40 +0000

    created callback func write for request to exec when fetch completes

--- fetcher.js
+++ fetcher.js
@@ -1,18 +1,18 @@
 const request = require("request");
 const input = process.argv.slice(2);
-const fs = require('fs')
-
+const fs = require('fs');
 
 const fetcher = function (userRequest) {
   let retrieve = userRequest[0];
   let store = userRequest[1];
   request(retrieve, (error, response, body) => {
-    console.log("This is body \n", body);
+    write(body);
   });
-  fs.writeFile(store, data, () => {
-    
-  });
-
+  const write = (data) => {
+    fs.writeFile(store, data, () => {
+      console.log("Downloaded and saved ${data} to ${store}");
+    });
+  };
 };
 
 fetcher(input);
