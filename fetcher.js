d4b78e687e43a2015d4298903027d018075e25a6
Angel Tsang <blacitea@gmail.com>
2020-07-30 05:52:04 +0000

    refactor writeFile part out from fetcher

--- fetcher.js
+++ fetcher.js
@@ -1,8 +1,7 @@
 const request = require("request");
 const input = process.argv.slice(2);
-const fs = require('fs');
-const readline = require('readline');
 const { stdout } = require("process");
+const { writeFile } = require("./write");
 
 
 
@@ -11,41 +10,14 @@ const fetcher = function (userRequest) {
   let retrieve = userRequest[0];
   let store = userRequest[1];
   request(retrieve, (error, response, body) => {
+    if (response.statusCode !== 200) {
+
+      console.log("Something went wrong!");
+      process.exit();
+    }
     console.log(response.statusCode);
-    fs.open(store, 'ax', (err, fd) => {
-      console.log(err);
-      if (err && err.code === 'EACCES') {
-        stdout.write("Invalid path for write file, please verify. \n");
-        process.exit();
-      }
-      if (err && err.code === 'EEXIST') {
-        const rl = readline.createInterface({
-          input: process.stdin,
-          output: process.stdout
-        });
-        rl.question('File already exist, overwrite? (yes / no)       ', (answer) => {
-          if (answer.toLowerCase() === 'yes') {
-            write(body);
-            rl.close();
-          } else {
-            console.log("Quit without saving.");
-            rl.close();
-          }
-        });
-      } else {
-        write(body);
-      }
-    });
+    writeFile(store, body);
   });
-  const write = (data) => {
-    fs.writeFile(store, data,  () => {
-      fs.stat(store/*why not retrieve*/, (err, stats) => {
-        // console.log(err);
-        // console.log(stats);
-        console.log(`Downloaded and saved ${stats.size} bytes to ${store}`);
-      });
-    });
-  };
 };
 
 fetcher(input);
---
+++ write.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+const readline = require('readline');
+const { stdout } = require("process");
+
+let writeFile = function (pathLink, data) {
+  fs.open(pathLink, 'ax', (err, fd) => {
+    //console.log(err);
+    if (err && err.code === 'EACCES') {
+      stdout.write("Invalid path for write file, please verify. \n");
+      process.exit();
+    }
+    if (err && err.code === 'EEXIST') {
+      const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+      });
+      rl.question('File already exist, overwrite? (yes / no)       ', (answer) => {
+        if (answer.toLowerCase() === 'yes') {
+          write(pathLink, data);
+          rl.close();
+        } else {
+          console.log("Quit without saving.");
+          rl.close();
+        }
+      });
+    } else {
+      write(pathLink, data);
+    }
+  });
+};
+
+const write = (pathLink, data) => {
+  fs.writeFile(pathLink, data,  () => {
+    fs.stat(pathLink/*why not retrieve*/, (err, stats) => {
+      // console.log(err);
+      // console.log(stats);
+      console.log(`Downloaded and saved ${stats.size} bytes to ${pathLink}`);
+    });
+  });
+};
+
+module.exports = {
+  writeFile,
+};
